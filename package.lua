51bc3e307e23f912308beb2ae33b7118528820f3
Lyrthras <defelixanne@gmail.com>
2023-01-11 02:29:28 +0800

    Add package.lua for lit installability

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+    name = "wbx/discordia-llslash",
+    version = "1.0.0",
+    description = "Low-level slash library for Discordia.",
+    license = "MIT",
+    dependencies = {
+        "https://github.com/Bilal2453/discordia-interactions"
+    }
+}
